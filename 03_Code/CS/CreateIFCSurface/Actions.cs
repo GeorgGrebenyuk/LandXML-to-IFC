32d03465429e5a2d2108cec0a749ed8e6b762326
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-05-03 00:41:18 +0300

    Create CS project

---
+++ 03_Code/CS/CreateIFCSurface/Actions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+
+
+namespace CreateIFCSurface
+{
+	public static class Actions
+	{
+		public static void ConvertOpeation (string PathToSourceFile, string PathToFinishFile, double [] CoordTransform)
+		{
+			string guid = Guid.NewGuid().ToString();
+			string IFC_FilePath = $@"C:\Users\GeorgKeneberg\Documents\Temp\Test-{guid}.ifc";
+			//Скопировали файл шаблона для последующего заполнения
+			File.Copy(@"D:\Programming\GitRepo\LandXML-to-IFC\02_Resources\ЦММ_Шаблон.ifc", IFC_FilePath);
+			//Добавили информацию о сдвижке координат
+			string InfoAboutLocation = $"#43= IFCCARTESIANPOINT(({CoordTransform[1]},{CoordTransform[0]},{CoordTransform[2]}));";
+			//string InfoAboutLocation = $"#43= IFCCARTESIANPOINT((0.,0.,0.));";
+			File.AppendAllText(IFC_FilePath, InfoAboutLocation + Environment.NewLine);
+
+			//Добавляем блок информации о точках (группа Pnts)
+			XDocument LandXML_Doc = XDocument.Load(PathToSourceFile);
+			XElement Pnts = LandXML_Doc.Descendants().Where(a => a.Name.LocalName == "Pnts").First();
+			long Counter1 = 1005;
+			foreach (XElement OnePoint in Pnts.Elements())
+			{
+				string[] PointCoordinates = OnePoint.Value.Split(' ');
+				string NewPointCoord = 
+					(Convert.ToDouble(PointCoordinates[0]) * 1000).ToString() + "," + 
+					(Convert.ToDouble(PointCoordinates[1]) * 1000).ToString() + "," + 
+					(Convert.ToDouble(PointCoordinates[2]) * 1000).ToString();
+				string InfoAboutPoint = $"#{Counter1}= IFCCARTESIANPOINT(({NewPointCoord}));" + Environment.NewLine;
+				File.AppendAllText(IFC_FilePath, InfoAboutPoint);
+				Counter1++;
+			}
+			//Добавляем блок информации о триангуляции (группа Faces)
+			XElement Faces = LandXML_Doc.Descendants().Where(a => a.Name.LocalName == "Faces").First();
+			string IFCOPENSHELL = "#675= IFCOPENSHELL((";
+			foreach (XElement OneFace in Faces.Elements())
+			{
+				string[] PointNums = OneFace.Value.Split(' ');
+				string NewPointsNums = 
+					"#" + (Convert.ToInt64(PointNums[0]) + 1000).ToString() + "," +
+					"#" + (Convert.ToInt64(PointNums[1]) + 1000).ToString() + "," +
+					"#" + (Convert.ToInt64(PointNums[2]) + 1000).ToString();
+
+				string IFCPOLYLOOP = $"#{Counter1}= IFCPOLYLOOP(({NewPointsNums}));" + Environment.NewLine;
+				string IFCFACEOUTERBOUND = $"#{Counter1+1}= IFCFACEOUTERBOUND(#{Counter1},.T.);" + Environment.NewLine;
+				string IFCFACE = $"#{Counter1 + 2}= IFCFACE((#{Counter1+1}));" + Environment.NewLine;
+				if (IFCOPENSHELL.Length < 22) IFCOPENSHELL += $"#{Counter1 + 2}";
+				else IFCOPENSHELL += $",#{Counter1 + 2}";
+
+				File.AppendAllText(IFC_FilePath, IFCPOLYLOOP + IFCFACEOUTERBOUND + IFCFACE);
+				//File.AppendAllText(IFC_FilePath, InfoAboutPoint);
+				Counter1 +=3;
+			}
+			//Добавляем блок информации о IFCOPENSHELL
+			File.AppendAllText(IFC_FilePath, IFCOPENSHELL + "));" + Environment.NewLine);
+
+			//Обязательная концовка файла
+			File.AppendAllText(IFC_FilePath, "ENDSEC;" + Environment.NewLine + "END-ISO-10303-21;" + Environment.NewLine);
+
+		}
+		/// <summary>
+		/// Инициализируем файл LandXML и выводим его BoundingBox + высоту
+		/// </summary>
+		/// <param name="PathToSourceFile"></param>
+		/// <returns>Массив со значениями, которыt прописываются в консоль приложения + записываются в Fields как рекомендованные паарметры трансформации</returns>
+		public static void CheckFileLocation (string PathToSourceFile)
+		{
+			XDocument LandXML_Doc = XDocument.Load(PathToSourceFile);
+			XElement Pnts = LandXML_Doc.Descendants().Where(a => a.Name.LocalName == "Pnts").First();
+
+			//IEnumerable<XNode> PntsCollection = LandXML_Doc.Descendants().Where(a => a.Name.LocalName == "Pnts");
+			//Временные переменные
+			double XMin = 100000000; double YMin = 100000000; double XMax = -100000000; double YMax = -100000000; double ZMin = 100000000; double ZMax = -100000000;
+			foreach (XElement OnePoint in Pnts.Elements())
+			{
+				string [] OnePointCoords = OnePoint.Value.Split(' ');
+				double X_Coord = Convert.ToDouble(OnePointCoords[1]);
+				double Y_Coord = Convert.ToDouble(OnePointCoords[0]);
+				double Z_Coord = Convert.ToDouble(OnePointCoords[2]);
+
+				if (X_Coord > XMax) XMax = X_Coord;
+				else if (X_Coord < XMin) X_Coord = XMin;
+
+				if (Y_Coord > YMax) YMax = Y_Coord;
+				else if (Y_Coord < YMin) Y_Coord = YMin;
+
+				if (Z_Coord > ZMax) ZMax = Z_Coord;
+				else if (Z_Coord < ZMin) Z_Coord = ZMin;
+			}
+
+			//double[] Offset = new double[3] { -(XMax + XMin) * 500, -(YMax + YMin) * 500, -(ZMax + ZMin) * 500 };
+			double[] Offset = new double[3] { -XMax*1000, -YMax * 1000, -ZMax * 1000,};
+			ConvertOpeation(MainWindow.PathToLandXMLFile, MainWindow.PathToIFCSaving, Offset);
+
+		}
+	}
+}
---
+++ 03_Code/CS/CreateIFCSurface/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace CreateIFCSurface
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			RB_3.IsEnabled = false;
+			TB_Point1.IsEnabled = false;
+			TB_Point2.IsEnabled = false;
+			TB_Point3.IsEnabled = false;
+		}
+		public static string PathToLandXMLFile = null;
+		public static string PathToIFCSaving = null;
+
+		private void Button_Click(object sender, RoutedEventArgs e) //Выбор файла LandXML
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == true) PathToLandXMLFile = openFileDialog.FileName;
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e) //Консоль приложения
+		{
+
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e) //Кнопка запуска процедуры конвертации
+		{
+			//Проверка файловых путей и опции выбора параметров преобразования
+			//if (RB_1.IsChecked == false && RB_2.IsChecked == false && RB_1.IsChecked == false) MessageBox.Show("Не выбрана опция обработки файла");
+			//if (!File.Exists(PathToLandXMLFile)) { MessageBox.Show("Файл LandXML не был выбран или путь недействительный"); PathToLandXMLFile = null; }
+			PathToLandXMLFile = @"D:\Programming\GitRepo\LandXML-to-IFC\02_Resources\K13-395_Surface.xml";
+			if (RB_1.IsChecked == true) Actions.ConvertOpeation(PathToLandXMLFile, PathToIFCSaving, new double[3] { 0d, 0d, 0d });
+			else if (RB_2.IsChecked == true) Actions.CheckFileLocation(PathToLandXMLFile);
+			ConsoleApp.Text = "End!";
+		}
+
+		private void TB_XField_TextChanged(object sender, TextChangedEventArgs e) //Поле координаты X
+		{
+
+		}
+
+		private void TB_YField_TextChanged(object sender, TextChangedEventArgs e) //Поле координаты Y
+		{
+
+		}
+
+		private void TB_ZField_TextChanged(object sender, TextChangedEventArgs e) //Поле координаты Z
+		{
+
+		}
+
+		private void Button_Click_2(object sender, RoutedEventArgs e) //Сохранение файла IFC
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			if (saveFileDialog.ShowDialog() == true) PathToIFCSaving = saveFileDialog.FileName;
+		}
+
+		private void RadioButton_Checked(object sender, RoutedEventArgs e) //Как есть
+		{
+
+		}
+
+		private void RadioButton_Checked_1(object sender, RoutedEventArgs e) //Трансфомация координат
+		{
+
+		}
+
+		private void RadioButton_Checked_2(object sender, RoutedEventArgs e) //Задавать сдвижку по рассчитанным значениям
+		{
+
+		}
+	}
+}
