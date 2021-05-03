42f5fbbb796ac963dca57c48099103572e9bfe37
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-05-03 19:08:04 +0300

    Update start interface

--- 03_Code/CS/CreateIFCSurface/MainWindow.xaml.cs
+++ 03_Code/CS/CreateIFCSurface/MainWindow.xaml.cs
@@ -25,9 +25,17 @@ namespace CreateIFCSurface
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			//Сугубо для отладки/тестирования
+			C_Point1.Text = "2216582.1221,530008.5171,136";
+			C_Point2.Text = "2216565.5541,530052.8739,136";
+			C_Point3.Text = "2216547.802,530046.2432,136";
+			F_Point1.Text = "-8.282,2.125,0";
+			F_Point2.Text = "39.068,2.125,0";
+			F_Point3.Text = "39.068,21.075,0";
 		}
 		public static string PathToLandXMLFile = null;
-		public static string PathToIFCSaving = null;
+
 		public static StringBuilder Log = new StringBuilder();
 
 		private void Button_Click(object sender, RoutedEventArgs e) //Выбор файла LandXML
@@ -43,27 +51,26 @@ namespace CreateIFCSurface
 
 		private void Button_Click_1(object sender, RoutedEventArgs e) //Кнопка запуска процедуры конвертации
 		{
+
+
 			//Проверка файловых путей и опции выбора параметров преобразования
-			//if (RB_1.IsChecked == false && RB_2.IsChecked == false && RB_1.IsChecked == false) MessageBox.Show("Не выбрана опция обработки файла");
-			//if (!File.Exists(PathToLandXMLFile)) { MessageBox.Show("Файл LandXML не был выбран или путь недействительный"); PathToLandXMLFile = null; }
+			if (RB_1.IsChecked == false && RB_2.IsChecked == false && RB_1.IsChecked == false) MessageBox.Show("Не выбрана опция обработки файла");
+			if (!File.Exists(PathToLandXMLFile)) { MessageBox.Show("Файл LandXML не был выбран или путь недействительный"); PathToLandXMLFile = null; }
 			PathToLandXMLFile = @"D:\Programming\GitRepo\LandXML-to-IFC\02_Resources\L15_500_Surface.xml";
-			if (RB_1.IsChecked == true) Actions.ConvertOpeation(PathToLandXMLFile, PathToIFCSaving, new double[4] { 0d, 0d, 0d, 0d },false);
+			if (RB_1.IsChecked == true) Actions.ConvertOpeation(PathToLandXMLFile, new double[4] { 0d, 0d, 0d, 0d },false);
 			else if (RB_2.IsChecked == true) Actions.CheckFileLocation(PathToLandXMLFile);
 			else if (RB_3.IsChecked == true)
 			{
+
+
 				string[] Data = new string[6];
-				//Data[0] = C_Point1.Text;
-				//Data[1] = C_Point2.Text;
-				//Data[2] = C_Point3.Text;
-				//Data[3] = F_Point1.Text;
-				//Data[4] = F_Point2.Text;
-				//Data[5] = F_Point3.Text;
-				Data[0] = "2216582.1221,530008.5171,136";
-				Data[1] = "2216565.5541,530052.8739,136";
-				Data[2] = "2216547.802,530046.2432,136";
-				Data[3] = "-8.282,2.125,0";
-				Data[4] = "39.068,2.125,0";
-				Data[5] = "39.068,21.075,0";
+				Data[0] = C_Point1.Text;
+				Data[1] = C_Point2.Text;
+				Data[2] = C_Point3.Text;
+				Data[3] = F_Point1.Text;
+				Data[4] = F_Point2.Text;
+				Data[5] = F_Point3.Text;
+
 				Actions.FindParameters(Data);
 			}
 			Log.Append(Environment.NewLine + "End!");
@@ -85,11 +92,6 @@ namespace CreateIFCSurface
 
 		}
 
-		private void Button_Click_2(object sender, RoutedEventArgs e) //Сохранение файла IFC
-		{
-			SaveFileDialog saveFileDialog = new SaveFileDialog();
-			if (saveFileDialog.ShowDialog() == true) PathToIFCSaving = saveFileDialog.FileName;
-		}
 
 		private void RadioButton_Checked(object sender, RoutedEventArgs e) //Как есть
 		{
